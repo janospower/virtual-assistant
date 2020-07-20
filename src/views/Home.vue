fd966d1a948dd89ba2898f6719a5b8e5136b0122
Janos <janos@goodpatch.com>
2020-07-20 15:12:07 +0200

    Log microphone levels

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,100 @@
 <template>
   <div class="home">
     <router-link to="/about">About</router-link>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-on:click="beginDetect">Start</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  export default{
+    data() {
+      return {
+        counter: 0,
+      }
+    },
+    methods: {
+      beginDetect() {
+        audioContext = new (window.AudioContext || window.webkitAudioContext)()
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+          navigator.mediaDevices.getUserMedia({audio: true}).then((stream) => {
+            mediaStreamSource = audioContext.createMediaStreamSource(stream)
+            meter = createAudioMeter(audioContext)
+            mediaStreamSource.connect(meter)
+          })
+        }
+      },
+    },
+  };
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+
+  var audioContext;
+  var mediaStreamSource = null
+  var meter = null
+
+
+  function createAudioMeter(audioContext, clipLevel, averaging, clipLag) {
+    const processor = audioContext.createScriptProcessor(512)
+    processor.onaudioprocess = volumeAudioProcess
+    processor.clipping = false
+    processor.lastClip = 0
+    processor.volume = 0
+    processor.clipLevel = clipLevel || 0.98
+    processor.averaging = averaging || 0.95
+    processor.clipLag = clipLag || 750
+
+    // this will have no effect, since we don't copy the input to the output,
+    // but works around a current Chrome bug.
+    processor.connect(audioContext.destination)
+
+    processor.checkClipping = function () {
+      if (!this.clipping) {
+        return false
+      }
+      if ((this.lastClip + this.clipLag) < window.performance.now()) {
+        this.clipping = false
+      }
+      return this.clipping
+    }
+
+    processor.shutdown = function () {
+      this.disconnect()
+      this.onaudioprocess = null
+    }
+
+    return processor
   }
-}
+
+  function volumeAudioProcess(event) {
+    const buf = event.inputBuffer.getChannelData(0)
+    const bufLength = buf.length
+    let sum = 0
+    let x
+
+    // Do a root-mean-square on the samples: sum up the squares...
+    for (var i = 0; i < bufLength; i++) {
+      x = buf[i]
+      if (Math.abs(x) >= this.clipLevel) {
+          this.clipping = true
+          this.lastClip = window.performance.now()
+      }
+      sum += x * x
+    }
+
+    // ... then take the square root of the sum.
+    const rms = Math.sqrt(sum / bufLength)
+
+    // Now smooth this out with the averaging factor applied
+    // to the previous sample - take the max here because we
+    // want "fast attack, slow release."
+    this.volume = Math.max(rms, this.volume * this.averaging)
+    console.log(this.volume);
+  }
 </script>
 
 <style scoped>
+
+button {
+  margin: 50px;
+}
 
 </style>
