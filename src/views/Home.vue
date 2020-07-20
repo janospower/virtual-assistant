0b2b0162d6e0b51d2502085e6cf0f8d4f09d9575
Janos <janos@goodpatch.com>
2020-07-20 16:45:42 +0200

    Mic level meter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <router-link to="/about">About</router-link>
-    <button v-on:click="beginDetect">Start</button>
+    <button @click="beginDetect">Start</button>
+    <div class="mic-meter" v-bind:style="{ width: vol + 'px' }"></div>
   </div>
 </template>
 
@@ -9,92 +10,81 @@
   export default{
     data() {
       return {
-        counter: 0,
+        audioContext: null,
+        mediaStreamSource: null,
+        meter: null,
+        vol: 100,
       }
     },
     methods: {
+      createAudioMeter(audioContext, clipLevel, averaging, clipLag) {
+        const processor = audioContext.createScriptProcessor(512)
+        processor.onaudioprocess = this.volumeAudioProcess
+        processor.volume = 0
+        processor.averaging = averaging || 0.95
+        processor.clipLag = clipLag || 750
+
+        // this will have no effect, since we don't copy the input to the output,
+        // but works around a current Chrome bug.
+        processor.connect(audioContext.destination)
+
+        processor.shutdown = function () {
+          this.disconnect()
+          this.onaudioprocess = null
+        }
+
+        return processor
+      },
       beginDetect() {
-        audioContext = new (window.AudioContext || window.webkitAudioContext)()
+        this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
           navigator.mediaDevices.getUserMedia({audio: true}).then((stream) => {
-            mediaStreamSource = audioContext.createMediaStreamSource(stream)
-            meter = createAudioMeter(audioContext)
-            mediaStreamSource.connect(meter)
+            this.mediaStreamSource = this.audioContext.createMediaStreamSource(stream)
+            this.meter = this.createAudioMeter(this.audioContext)
+            this.mediaStreamSource.connect(this.meter)
           })
         }
       },
+      volumeAudioProcess(event) {
+        const buf = event.inputBuffer.getChannelData(0)
+        const bufLength = buf.length
+        let sum = 0
+        let x
+        let _this = event.currentTarget;
+
+        // Do a root-mean-square on the samples: sum up the squares...
+        for (var i = 0; i < bufLength; i++) {
+          x = buf[i]
+          sum += x * x
+        }
+
+        // ... then take the square root of the sum.
+        const rms = Math.sqrt(sum / bufLength)
+
+        // Now smooth this out with the averaging factor applied
+        // to the previous sample - take the max here because we
+        // want "fast attack, slow release."
+        _this.volume = Math.max(rms, _this.volume * _this.averaging)
+
+        this.vol = _this.volume * 10000;
+
+      },
     },
   };
-
-
-  var audioContext;
-  var mediaStreamSource = null
-  var meter = null
-
-
-  function createAudioMeter(audioContext, clipLevel, averaging, clipLag) {
-    const processor = audioContext.createScriptProcessor(512)
-    processor.onaudioprocess = volumeAudioProcess
-    processor.clipping = false
-    processor.lastClip = 0
-    processor.volume = 0
-    processor.clipLevel = clipLevel || 0.98
-    processor.averaging = averaging || 0.95
-    processor.clipLag = clipLag || 750
-
-    // this will have no effect, since we don't copy the input to the output,
-    // but works around a current Chrome bug.
-    processor.connect(audioContext.destination)
-
-    processor.checkClipping = function () {
-      if (!this.clipping) {
-        return false
-      }
-      if ((this.lastClip + this.clipLag) < window.performance.now()) {
-        this.clipping = false
-      }
-      return this.clipping
-    }
-
-    processor.shutdown = function () {
-      this.disconnect()
-      this.onaudioprocess = null
-    }
-
-    return processor
-  }
-
-  function volumeAudioProcess(event) {
-    const buf = event.inputBuffer.getChannelData(0)
-    const bufLength = buf.length
-    let sum = 0
-    let x
-
-    // Do a root-mean-square on the samples: sum up the squares...
-    for (var i = 0; i < bufLength; i++) {
-      x = buf[i]
-      if (Math.abs(x) >= this.clipLevel) {
-          this.clipping = true
-          this.lastClip = window.performance.now()
-      }
-      sum += x * x
-    }
-
-    // ... then take the square root of the sum.
-    const rms = Math.sqrt(sum / bufLength)
-
-    // Now smooth this out with the averaging factor applied
-    // to the previous sample - take the max here because we
-    // want "fast attack, slow release."
-    this.volume = Math.max(rms, this.volume * this.averaging)
-    console.log(this.volume);
-  }
 </script>
 
 <style scoped>
 
-button {
-  margin: 50px;
-}
+  button {
+    margin: 50px;
+  }
+
+  .mic-meter {
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    bottom: 0px;
+    background-color: hsla(0,0%,100%,0.8);
+  }
 
 </style>
